c5bbac97e1bea3589be95dc2a3400343667e3bc9
anh vu <vu.doan.vtp@gmail.com>
2023-11-14 12:33:03 +0700

    Bai 2 - Quan ly sinh vien

---
+++ Bai_2/src/SinhVienChinhQuy.java
@@ -0,0 +1,9 @@
+
+public class SinhVienChinhQuy extends SinhVien {
+	
+	public SinhVienChinhQuy(String maSinhVien, String hoTen, String ngaySinh, int namVaoHoc, double diemDauVao) {
+		super(maSinhVien, hoTen, ngaySinh, namVaoHoc, diemDauVao);
+	}
+	
+	
+}
